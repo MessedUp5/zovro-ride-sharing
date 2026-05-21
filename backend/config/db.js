1fcca296296f04c3c3ac56f4e99abe54790575cc
Vidurshan <Vidurshan145@gmail.com>
2026-05-21 10:52:45 +0530

    New

--- backend/config/db.js
+++ backend/config/db.js
@@ -17,7 +17,7 @@ const pool = process.env.DATABASE_URL
     });
 
 pool.on('connect', () => {
-    console.log('Connected to PostgreSQL database successfully');
+    console.log('Connected to PostgreSQL db successfully');
 });
 
 module.exports = pool;
